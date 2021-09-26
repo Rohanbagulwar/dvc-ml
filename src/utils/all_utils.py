cce763f358df5a537d276ad13cff07100ab56919
ideapad GAMIN <rohanbagulwarrb@gmail.com>
2021-09-26 09:59:49 +0530

    dvc repro completion

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,7 +2,12 @@ import yaml
 import os
 
 def read_yaml(path_to_yaml: str) -> dict:
-    with open(path_to_yaml, 'r') as yaml_file:
+    with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path)
+        print(f'directory is created at {dir_path}')
 
